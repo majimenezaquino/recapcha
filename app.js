26ccfa9c55ef1b494be58efd6a547419f2f413d2
Miguel Jimenez <migueljimenezaquino@gmail.com>
2020-03-17 11:36:16 -0400

    add function image

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 let secretKey = null;
-let emailMaster = null
+let emailMaster = null;
 //"6LeEe-AUAAAAAEWTxAlBnbKgzvUmORdyxnRxeYml"
-const {
-  AccountRepository,
-  RecaptchaRepository
-} = require("./repositories");
+const nodemailer = require("nodemailer");
+
+const { AccountRepository, RecaptchaRepository } = require("./repositories");
 
 const app = express();
 
@@ -17,7 +16,7 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
@@ -29,7 +28,6 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
-
 });
 
 app.post("/webmail", async (req, res) => {
@@ -40,27 +38,39 @@ app.post("/webmail", async (req, res) => {
       msg: "Capctha is not checked"
     });
   }
-  let hostReq = req.get("host")
-  const recaptchaDb = await RecaptchaRepository.getrecaptcha(hostReq)
+  let hostReq = req.get("host");
+  const recaptchaDb = await RecaptchaRepository.getrecaptcha(hostReq);
   if (recaptchaDb && recaptchaDb.length) {
     for (let reca of recaptchaDb) {
-      if (reca["recaptcha_website_key"])
-        emailMaster
-      secretKey = reca["client_email_default"]
+      if (reca["recaptcha_website_key"]) {
+        secretKey = reca["recaptcha_website_key"];
+      }
+
+      if (
+        reca["client_email_default"] &&
+        validarEmail(reca["client_email_default"])
+      ) {
+        emailMaster = reca["client_email_default"];
+      } else {
+        emailMaster = `info@${hostReq}`;
+      }
+
+      ///================
+
+      //===================
     }
   } else {
     return res.status(401).json({
       success: false,
       msg: "website not allower"
     });
-
   }
   let htmlEmail = `<body>
 <div class="email-example">
   <h3 style="font-family: arial; color:#009fc2; text-align:center">Please keep this for your records.</h3>
   <td align="center" valign="top">
     <table border="0" style="font-family: arial; color:#565458;" cellpadding="20" cellspacing="0" width="100%" id="Body">
-`
+`;
 
   for (let key in req.body) {
     if (key == "captcha") continue;
@@ -73,13 +83,13 @@ app.post("/webmail", async (req, res) => {
       ${req.body[key]}
     </td>
   </tr>
-  `
+  `;
   }
   htmlEmail += `
   </table>
 </div>
 
-</body>`
+</body>`;
 
   const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}`;
 
@@ -102,9 +112,25 @@ app.post("/webmail", async (req, res) => {
         score: body.score
       });
     }
-
+    sendMail({
+      mailserver: {
+        host: "mail.asipr.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "test@mail.asipr.com",
+          pass: "usr123*1"
+        }
+      },
+      mail: {
+        from: "test@mail.asipr.com",
+        to: "migueljimenezaquino@gmail.com",
+        subject: "Hey",
+        html: htmlEmail
+      }
+    }).catch(console.error);
     // return json message or continue with your function. Example: loading new page, ect
-    return res.send(htmlEmail)
+    return res.send(htmlEmail);
   });
 });
 
@@ -112,11 +138,20 @@ app.listen(8083, () => {
   console.log("server is now up!");
 });
 
-
 function validarEmail(valor) {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor)) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 }
+
+const sendMail = async ({ mailserver, mail }) => {
+  // create a nodemailer transporter using smtp
+  let transporter = nodemailer.createTransport(mailserver);
+
+  // send mail using transporter
+  let info = await transporter.sendMail(mail);
+
+  console.log(`Preview: ${nodemailer.getTestMessageUrl(info)}`);
+};
